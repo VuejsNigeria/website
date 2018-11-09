b930d81981f85fcb03a2f4da95d883c144757a7e
BELLO MUSA <Musbell007@gmail.com>
2018-11-09 11:17:41 +0100

    implemented PWA

--- components/landingPage/eventCard.vue
+++ components/landingPage/eventCard.vue
@@ -31,11 +31,11 @@
               flat
               color="teal">Explore</v-btn>
             <v-spacer/>
-            <!--<v-btn-->
-            <!--icon-->
-            <!--@click="show = !show">-->
-            <!--<v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>-->
-            <!--</v-btn>-->
+            <v-btn
+              icon
+              @click="show = !show">
+              <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+            </v-btn>
           </v-card-actions>
 
           <v-slide-y-transition>
--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ module.exports = {
   ** Nuxt.js modules
   */
   modules: [
+    '@nuxtjs/pwa'
   ],
 
   /*
